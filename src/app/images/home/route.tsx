629323e27e230c982debacd1a4aa8b456f170859
gregfromstl <gregfromstl@gmail.com>
2024-01-31 22:32:56 -0800

    fuck sharp

--- src/app/images/home/route.tsx
+++ src/app/images/home/route.tsx
@@ -1,9 +1,8 @@
-import satori from "satori";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { join } from "path";
 import * as fs from "fs";
-import sharp from "sharp";
 import { revalidatePath } from "next/cache";
+import { ImageResponse } from "next/og";
 export const dynamic = "force-dynamic";
 
 const fontPath = join(process.cwd(), "PressStart.ttf");
@@ -12,31 +11,33 @@ let fontData = fs.readFileSync(fontPath);
 export async function GET(request: NextRequest) {
     revalidatePath(request.url);
 
-    const svg = await satori(
-        <div
-            style={{
-                color: "black",
-                width: "100%",
-                height: "100%",
-                backgroundColor: "white",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "16px",
-                gap: "32px",
-            }}
-        >
-            <img
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
+    return new ImageResponse(
+        (
+            <div
                 style={{
-                    width: "408px",
-                    height: "150px",
+                    color: "black",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "white",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "16px",
+                    gap: "32px",
                 }}
-                alt="Pokemon"
-            />
-            What&apos;s your FID&apos;s Pokemon?
-        </div>,
+            >
+                <img
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
+                    style={{
+                        width: "408px",
+                        height: "150px",
+                    }}
+                    alt="Pokemon"
+                />
+                What&apos;s your FID&apos;s Pokemon?
+            </div>
+        ),
         {
             width: 600,
             height: 400,
@@ -50,13 +51,4 @@ export async function GET(request: NextRequest) {
             ],
         }
     );
-    const pngBuffer = await sharp(Buffer.from(svg)).toFormat("png").toBuffer();
-
-    return new NextResponse(pngBuffer, {
-        status: 200,
-        headers: {
-            "Content-Type": "image/png",
-            "Cache-Control": "max-age=10",
-        },
-    });
 }
--- src/app/images/result/route.tsx
+++ src/app/images/result/route.tsx
@@ -1,8 +1,8 @@
 import satori from "satori";
 import { NextRequest, NextResponse } from "next/server";
+import { ImageResponse } from "next/og";
 import { join } from "path";
 import * as fs from "fs";
-import sharp from "sharp";
 import { revalidatePath } from "next/cache";
 export const dynamic = "force-dynamic";
 
@@ -16,34 +16,36 @@ export async function GET(request: NextRequest) {
     const image = decodeURIComponent(searchParams.get("image") ?? "");
     const id = parseInt(searchParams.get("id") ?? "0");
 
-    const svg = await satori(
-        <div
-            style={{
-                color: "black",
-                width: "100%",
-                height: "100%",
-                backgroundColor: "white",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                fontSize: "12px",
-                gap: "0px",
-            }}
-        >
-            <img
-                src={
-                    image ||
-                    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
-                }
+    return new ImageResponse(
+        (
+            <div
                 style={{
-                    width: image ? "200px" : "408px",
-                    height: image ? "200px" : "150px",
+                    color: "black",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "white",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    fontSize: "12px",
+                    gap: "0px",
                 }}
-                alt={name || "Pokemon"}
-            />
-            Your Pokemon is {name || "unknown"} (ID: {id})
-        </div>,
+            >
+                <img
+                    src={
+                        image ||
+                        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
+                    }
+                    style={{
+                        width: image ? "200px" : "408px",
+                        height: image ? "200px" : "150px",
+                    }}
+                    alt={name || "Pokemon"}
+                />
+                Your Pokemon is {name || "unknown"} (ID: {id})
+            </div>
+        ),
         {
             width: 600,
             height: 400,
@@ -57,13 +59,4 @@ export async function GET(request: NextRequest) {
             ],
         }
     );
-    const pngBuffer = await sharp(Buffer.from(svg)).toFormat("png").toBuffer();
-
-    return new NextResponse(pngBuffer, {
-        status: 200,
-        headers: {
-            "Content-Type": "image/png",
-            "Cache-Control": "max-age=10",
-        },
-    });
 }
