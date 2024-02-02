d32e32ecc7fb005bd936fa1f349b53886cc0a47c
gregfromstl <gregfromstl@gmail.com>
2024-02-02 09:40:15 -0800

    update devcaster

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
-import { framesMiddleware } from "@devcaster/next/frames";
 import type { NextRequest } from "next/server";
+import { framesMiddleware } from "@devcaster/next/frames";
 
 export async function middleware(request: NextRequest) {
     return await framesMiddleware(request);
