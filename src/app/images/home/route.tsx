4a60a3ef5bcff6bff7a256fb01896acf52ff6538
gregfromstl <gregfromstl@gmail.com>
2024-02-01 16:09:06 -0800

    re-add sharp

--- src/app/images/home/route.tsx
+++ src/app/images/home/route.tsx
@@ -1,8 +1,9 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { join } from "path";
 import * as fs from "fs";
 import { revalidatePath } from "next/cache";
-import { ImageResponse } from "next/og";
+import satori from "satori";
+import sharp from "sharp";
 export const dynamic = "force-dynamic";
 
 const fontPath = join(process.cwd(), "PressStart.ttf");
@@ -11,33 +12,31 @@ let fontData = fs.readFileSync(fontPath);
 export async function GET(request: NextRequest) {
     revalidatePath(request.url);
 
-    return new ImageResponse(
-        (
-            <div
+    const svg = await satori(
+        <div
+            style={{
+                color: "black",
+                width: "100%",
+                height: "100%",
+                backgroundColor: "white",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                fontSize: "16px",
+                gap: "32px",
+            }}
+        >
+            <img
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
                 style={{
-                    color: "black",
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor: "white",
-                    display: "flex",
-                    flexDirection: "column",
-                    alignItems: "center",
-                    justifyContent: "center",
-                    fontSize: "16px",
-                    gap: "32px",
+                    width: "408px",
+                    height: "150px",
                 }}
-            >
-                <img
-                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png"
-                    style={{
-                        width: "408px",
-                        height: "150px",
-                    }}
-                    alt="Pokemon"
-                />
-                What&apos;s your FID&apos;s Pokemon?
-            </div>
-        ),
+                alt="Pokemon"
+            />
+            What&apos;s your FID&apos;s Pokemon?
+        </div>,
         {
             width: 600,
             height: 400,
@@ -51,4 +50,16 @@ export async function GET(request: NextRequest) {
             ],
         }
     );
+
+    // Convert SVG to PNG using Sharp
+    const pngBuffer = await sharp(Buffer.from(svg)).toFormat("png").toBuffer();
+
+    // Set the content type to PNG and send the response
+    return new NextResponse(pngBuffer, {
+        status: 200,
+        headers: {
+            "Content-Type": "image/png",
+            "Cache-Control": "max-age=10",
+        },
+    });
 }
